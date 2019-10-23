08d3488638cc0dadfa26b09354a06283fbb57de6
“Jingqi <jessieyao0602@gmail.com>
2019-10-22 21:09:05 -0700

    problem1

--- problems/problem1/answer1.py
+++ problems/problem1/answer1.py
@@ -1,7 +1,16 @@
 class Solution(object):
     def reverse(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
+        inputed_number = input()
+        reversed_number = ''
+        if inputed_number[0:1] == '-':
+            reversed_number += '-'
+            inputed_number = inputed_number[1:]
+
+        for i in range(len(inputed_number)):
+            reversed_number += inputed_number[len(inputed_number)-i-1:len(inputed_number)-i]
+        reversed_number = int(reversed_number)
+        print(reversed_number)
+
+
+        
         return None
